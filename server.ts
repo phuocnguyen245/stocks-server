357954aba2b1fd8d6aab1c34aa55921f9577d7ed
Nguyen Truong <nguyentp@bestpicktechnology.com>
2024-01-24 23:05:04 +0700

    update deploy

---
+++ server.ts
@@ -0,0 +1,15 @@
+import app from './app.ts'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = process.env.PORT || 8080
+
+const server = app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`)
+})
+
+process.on('SIGINT', () => {
+  server.close(() => {
+    console.log('Server shut down')
+  })
+})
