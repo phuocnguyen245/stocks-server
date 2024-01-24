16fb59abd15e61c77ea81d80922f5e3c7f5b8d88
Nguyen Truong <nguyentp@bestpicktechnology.com>
2024-01-24 23:18:13 +0700

    update deploy

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`)
 })
 
-process.on('SIGINT', () => {
+app.on('SIGINT', () => {
   server.close(() => {
     console.log('Server shut down')
   })
